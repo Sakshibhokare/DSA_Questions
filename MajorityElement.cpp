0c29758f9318fddc8420bb0207e5c47e557437d3
Sakshi Suresh Bhokare <bhokaresakshi93@gmail.com>
2023-09-26 18:43:32 +0530

    Create MajorityElement.cpp

---
+++ MajorityElement.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        // brute force 
+//         for (int i = 0; i < n; i++) {
+//         //selected element is v[i]
+//         int cnt = 0;
+//         for (int j = 0; j < n; j++) {
+//             // counting the frequency of v[i]
+//             if (v[j] == v[i]) {
+//                 cnt++;
+//             }
+//         }
+
+//         // check if frquency is greater than n/2:
+//         if (cnt > (n / 2))
+//             return v[i];
+//     }
+
+        // first approach 
+        // unordered_map<int,int>mp;
+        // for(int i=0; i<nums.size(); i++){
+        //     mp[nums[i]]++;
+        // }
+        // for(auto it:mp){
+        //     if(it.second>nums.size()/2){
+        //         return it.first;
+        //     }
+        // }
+        // return -1;
+        
+       
+        
+    }
+};
